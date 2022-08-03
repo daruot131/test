4810761f4f69deb9a36c61d0ec8fd66a93c0cc10
tuananh <trantuananh.9.9.2014@gmail.com>
2022-08-03 16:45:04 +0700

    test 2

---
+++ src/demo/Test2.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package demo;
+
+/**
+ * @author tuananh
+ *
+ */
+public class Test2 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("bbbbbbb");
+        // TODO Auto-generated method stub
+
+    }
+
+}
